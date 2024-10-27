52616297b80b0f0e48d83699f6843d01b3339c89
@MHuyHoat <maihuyhoat@gmail.com>
2024-10-27 23:51:26 +0700

    .

--- api/ThietBiApi.php
+++ api/ThietBiApi.php
@@ -14,7 +14,7 @@ try {
     
             // Decode the JSON data into a PHP associative array
             $data = json_decode($postData, true);
-        if ($data['trangThaiThietBi']) {
+        if ($data['action']=="controlTrangThaiByButton") {
             $thietbi = $thietBiModel->find(['tb.id = ' => $data['id']]);
             $trangThaiThietBi = $thietbi['trangthai'];
             if ($trangThaiThietBi == 1) {
@@ -29,12 +29,69 @@ try {
             ];
             // Chuyển đổi dữ liệu thành JSON và trả về
             echo json_encode($responseData);
+        }else if($data['action']=="controlTrangThaiBySpeech"){
+            $text = mb_strtolower($data['text'], 'UTF-8');
+            $dataThietBi = $thietBiModel->getAll(['user_id' => $data['userId']]);
+            $thietBiThayDoi = null;
+            
+            foreach ($dataThietBi as $key => $value) {
+                // Chuyển đổi tên thiết bị sang chữ thường
+                $tenThietBi = mb_strtolower(trim($value['ten_thiet_bi']), 'UTF-8');
+                
+                // Kiểm tra xem $text có chứa $tenThietBi không
+                if (strstr($text, $tenThietBi)) {
+                    $thietBiThayDoi = $value;
+                    break;
+                }
+            }
+            if(empty($thietBiThayDoi)){
+                $responseData = [
+                    'status' => 'error',
+
+                    'message' =>"Lỗi không tìm thấy thiết bị muốn thay đổi ",
+                   
+                ];
+                // Chuyển đổi dữ liệu thành JSON và trả về
+                echo json_encode($responseData);
+                
+            }else{
+                if(strstr($text,'bật')){
+                    $thietBiModel->update($thietBiThayDoi['id'], ['trangthai = ' => 1]);
+                    $responseData = [
+                        'status' => 'success',
+                        'message' =>"Đã thay đổi thành công trạng thái thiết bị ",
+                        'data'=> $thietBiModel->find(['tb.id = ' =>  $thietBiThayDoi['id']])
+                    ];
+                    // Chuyển đổi dữ liệu thành JSON và trả về
+                    echo json_encode($responseData);
+                }else if(strstr($text,'tắt')){
+                    $thietBiModel->update($thietBiThayDoi['id'], ['trangthai = ' => 0]);
+                    $responseData = [
+                        'status' => 'success',
+                        'message' =>"Đã thay đổi thành công trạng thái thiết bị ",
+                        'data'=> $thietBiModel->find(['tb.id = ' =>  $thietBiThayDoi['id']])
+                    ];
+                    // Chuyển đổi dữ liệu thành JSON và trả về
+                    echo json_encode($responseData);
+                }else{
+                    $responseData = [
+                        'status' => 'error',
+                        'message' =>"Lỗi không rõ yêu cầu với thiết bị ",
+                       
+                    ];
+                    // Chuyển đổi dữ liệu thành JSON và trả về
+                    echo json_encode($responseData);
+                }
+
+            }
+            
         }
     } else {
         echo json_encode([
             'status' => 'error',
             'message' => 'Phương thức không hợp lệ.'
         ]);
+        
     }
 } catch (\Throwable $th) {
     $responseData = [
--- assets/js/home.js
+++ assets/js/home.js
@@ -1,48 +1,88 @@
-function speech() {
-    // lấy tham chiếu output div
-    var output = document.getElementById("output");
-    // lấy tham chiếu action element
-    var action = document.getElementById("action");
-    // Đối tượng nhận dạng giọng nói mới
-    var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
-    var recognition = new SpeechRecognition();
-    // cài đặt ngôn ngữ 
-    recognition.lang = 'vi-VN';
-    // Điều này sẽ chạy khi nhận dạng giọng nói bắt đầu
-    recognition.onstart = function() {
-        action.innerHTML = "<small><br>Đang lắng nghe....Hãy nói </small>";
-    };
+function startSpeech() {
+  // lấy tham chiếu output div
+  var output = document.getElementById("speech-text");
+  // Đối tượng nhận dạng giọng nói mới
+  var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+  var recognition = new SpeechRecognition();
+  // cài đặt ngôn ngữ
+  recognition.lang = "vi-VN";
+  // Điều này sẽ chạy khi nhận dạng giọng nói bắt đầu
+  recognition.onstart = function () {
+    output.innerHTML = "Đang lắng nghe...";
+  };
 
-    recognition.onspeechend = function() {
-        action.innerHTML = "<small><br>Ngừng nghe, hy vọng bạn đã xong ...</small>";
-        recognition.stop();
-    }
+ 
 
-    // Điều này sẽ chạy khi nhận dạng giọng nói trả về kết quả
-    recognition.onresult = function(event) {
-        var transcript = event.results[0][0].transcript;
-         
-        output.innerHTML = "<b>Hiển thị:</b> " + transcript + "<br/>";
-        document.getElementById('textt').setAttribute('value', transcript);
+  // Điều này sẽ chạy khi nhận dạng giọng nói trả về kết quả
+  recognition.onresult = function (event) {
+    var transcript = event.results[0][0].transcript;
 
-        output.classList.remove("hide");
+    output.innerHTML = transcript;
+    document
+      .getElementById("speech-text-value")
+      .setAttribute("value", transcript);
 
-    };
-    recognition.start();
+  };
+  recognition.start();
 }
-function setTrangThaiThietBi(id){
-    fetch('api/ThietBiApi.php',{
-        method:'post',
-        headers:{
-            'Content-type':'application/json',
-            'Accept':'*'
-        },
-        body:JSON.stringify({
-            "id": id,
-            "trangThaiThietBi": true
-        })
-    }).then(res=>res.json())
-    .then(data=>{
-        console.log(data)
-    }).catch(err=>console.log(err));
+
+function stopSpeech() {
+  
+  document.getElementById("speech-text-value").setAttribute("value", "");
+  
+}
+function setTrangThaiThietBiBoiGiongNoi(userId){
+  var text=   document
+  .getElementById("speech-text-value").value;
+  fetch("api/ThietBiApi.php", {
+    method: "post",
+    headers: {
+      "Content-type": "application/json",
+      Accept: "*",
+    },
+    body: JSON.stringify({
+      userId:userId,
+      text:text,
+      action: 'controlTrangThaiBySpeech',
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.status == "success") {
+        toastr.success("Bạn đã đổi trạng thái thiết bị thành công ");
+        console.log(data);
+        document.querySelector(`#statusTrangThai${data.data.id}`).checked= data.data.trangthai==1?true:false ;
+        document.querySelector('#cancelModalSpeech').click();
+      } else {
+        toastr.error(data.message);
+         
+      }
+    })
+    .catch((err) => console.log(err));
+  
+}
+function setTrangThaiThietBi(id) {
+  fetch("api/ThietBiApi.php", {
+    method: "post",
+    headers: {
+      "Content-type": "application/json",
+      Accept: "*",
+    },
+    body: JSON.stringify({
+      id: id,
+      action: 'controlTrangThaiByButton',
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.status == "success") {
+        toastr.success("Bạn đã đổi trạng thái thiết bị thành công ");
+     
+      } else {
+        toastr.error("Có lỗi xảy ra. Vui lòng thử lại sau");
+       
+        window.location.reload();
+      }
+    })
+    .catch((err) => console.log(err));
 }
