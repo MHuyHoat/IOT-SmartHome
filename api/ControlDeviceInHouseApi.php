a96daef88fec836a3d344f73bbd02414eb0cdc6e
@MHuyHoat <maihuyhoat@gmail.com>
2024-11-06 11:22:27 +0700

    .

--- api/ControlDeviceInHouseApi.php
+++ api/ControlDeviceInHouseApi.php
@@ -3,6 +3,7 @@
 try {
   require_once(__DIR__ . '/../helpers/Helpers.php');
   require_once(__DIR__ . '/../models/ThietBi.php');
+  require_once(__DIR__ . '/../config/DBConn.php');
   header('Content-Type: application/json');
 
   $helper = new Helpers();
@@ -10,9 +11,9 @@ try {
   //echo "found";  
   // lấy toàn bộ các thiết bị trong nhà 
   $thietBiModel = new ThietBi();
-  $data = $thietBiModel->getAll([
-    'tb.parent_id =' => $decodeJWT['id'],
-  ]);
+  $chipConnect= $thietBiModel->find(['tb.id ='=>$decodeJWT['id']]);
+  $conn= new DBConn();
+  $data = $conn->executeQuery("SELECT * FROM thietbi WHERE nha_id=".$chipConnect['nha_id']."")->fetchAll();
 
   $jsonString = json_encode($data);
   echo $jsonString;
